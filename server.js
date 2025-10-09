477422753e7b970824b3f82951073f10b03665bf
lucasferrre <lucasferreirasasuke@gmail.com>
2025-10-09 08:29:32 -0300

    finalizando o exercicio1

--- server.js
+++ server.js
@@ -9,11 +9,18 @@ app.use(express.json());
 app.post('/mensagem', (req, res) => {
     try {
         //desestruturação
-        const {nome, idade, time_favorito} = req.body; // no post a gente requere ele com body que é o corpo da rquisição
-        console.log(`Olá ${nome}! Você tem ${idade} anos e torce para o ${time_favorito}!`);
+        const {nome, idade, timeFavorito} = req.body; // no post a gente requere ele com body que é o corpo da rquisição
+       if (typeof nome == "string" && !isNaN(idade) && typeof timeFavorito === "string") {
+        console.log(`Dados recebidos! Olá ${nome}! Você tem ${idade} anos e torce para o ${timeFavorito}!`);
         // coloco o status 201 pois quando faço um post eu 
         // estou criando uma novo registro/requisição, sempre que usarmos post usamos o 201
-        res.status(201).json ({message:`Dados recebidos com sucesso no servidor.`})
+        res.status(201).json ({message:`Dados recebidos com sucesso no servidor. Olá ${nome}! Você tem ${idade} anos e torce para o ${timeFavorito}!`});
+       }else{
+        console.log(`Dados inválidos!`);
+        // coloco o status 400 pois aqui é um erro de sintaxe e digitação do usuário
+        res.status(400).json ({message:`Dados inválidos! por favor, tente novamente!`})
+       }
+       
     } catch (error) {
         console.error(error);
         res.status(500).json({message:'Ocorreu um erro ao processar a solicitação!',
