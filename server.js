63ddc5f7ae67b79ca59be9b41f2aa0d2ba0aeb2e
lucasferrre <lucasferreirasasuke@gmail.com>
2025-10-09 08:08:51 -0300

    finalizando o exercicio3

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 8081;
 
-// midleware - é um recurso que fica entre a requisição e a resposta
+
 // nesse caso eu quero que o express entenda json eu coloco esse midleware para valer para todas as rotas
 app.use(express.json());
 
@@ -12,7 +12,7 @@ async function validaUsuario(pUsuario, pSenha) {
         if (pUsuario === 'admin' && pSenha === 1234) {
             return true;
         } else {
-            return false
+            return false;
         }
 
     } catch (error) {
@@ -21,15 +21,23 @@ async function validaUsuario(pUsuario, pSenha) {
 }
 
 
-
+// rota para o login
 app.post('/login', async (req,res) => {
     try {
         const {usuario, senha} = req.body;
         const resultado = await validaUsuario(usuario, senha);
-        console.log(`Usuário validado com sucesso! bem vindo ${usuario}!`);
-        // coloco o status 201 pois quando faço um post eu 
-        // estou criando uma novo registro/requisição, sempre que usarmos post usamos o 201
-        res.status(201).json ({message:`Dados recebidos com sucesso no servidor.`});
+        if (resultado === true) {
+            console.log(`Usuário validado com sucesso!`); 
+             // coloco o status 201 pois quando faço um post eu 
+            // estou criando uma novo registro/requisição, sempre que usarmos post usamos o 201
+            res.status(201).json ({message:`Dados recebidos com sucesso no servidor. Seja bem vindo ${usuario}!`});  
+        }else{
+            console.log(`Usuário inválido!`);
+             // coloco o status 201 pois quando faço um post eu 
+            // estou criando uma novo registro/requisição, sempre que usarmos post usamos o 201
+            res.status(400).json ({message:`Servidor não pode processar sua solicitação devido a um erro de sintaxe inválida, tente novamente!`});
+        }
+      
     } catch (error) {
         console.error(error);
         res.status(500).json({message:'Ocorreu um erro ao processar a solicitação!',
